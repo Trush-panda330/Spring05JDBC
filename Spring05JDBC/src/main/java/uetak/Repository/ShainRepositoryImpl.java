b446878111ea219ceea8a874fc73ed5235c8bf46
uetak330 <uetak330@gmail.com>
2024-09-30 17:46:24 +0900

    全社員を表示するメソッドの定義

--- Spring05JDBC/src/main/java/uetak/Repository/ShainRepositoryImpl.java
+++ Spring05JDBC/src/main/java/uetak/Repository/ShainRepositoryImpl.java
@@ -1,9 +1,10 @@
 package uetak.Repository;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -12,60 +13,43 @@ import uetak.entity.Shain;
 @Repository
 @Qualifier("ShainRepositoryImpl")
 public class ShainRepositoryImpl implements ShainRepository {
-	
+
 	@Autowired
 	private NamedParameterJdbcTemplate jdbcTemplate;
 
 	@Override
 	public void findAll() {
-		
-		//社員リストを作る
-		ArrayList<Shain> shainList = new ArrayList<>();
-		
-		//社員１を作る
-		Shain shain1 = new Shain();
-		shain1.setId(101);
-		shain1.setName("鈴木義信");
-		shain1.setGender("男");
-		shain1.setNen(2003);
-		shain1.setAddress("宮城県仙台市");
-		//作ったインスタンスをリストに追加
-		shainList.add(shain1);
-		
-		
-		Shain shain2 = new Shain();
-		shain2.setId(102);
-		shain2.setName("佐藤香織");
-		shain2.setGender("女");
-		shain2.setNen(2004);
-		shain2.setAddress("福岡県福岡市");
-		shainList.add(shain2);
-		
-		for(Shain shain : shainList) {
-			System.out.println(shain.getId() + ":" +  shain.getName() 
-			+ ":" + shain.getGender() + ":" + shain.getNen() 
-			+ ":" + shain.getAddress());
+		//SQL文の作成
+		final String sql = "select id, name, gender, nen, address from shain";
+
+		//SQLの実行
+		List<Shain> shainList = jdbcTemplate.query(sql, new BeanPropertyRowMapper<Shain>(Shain.class));
+
+		for (Shain shain : shainList) {
+			System.out.println(shain.getId() + ":" + shain.getName() + ":"
+					+ shain.getGender() + ":" + shain.getNen() + ":" +shain.getAddress());
+
 		}
 	}
 
 	@Override
 	public void insertShain() {
-		
+
 	}
 
 	@Override
 	public void findByShain() {
-		
+
 	}
 
 	@Override
 	public void updateShain() {
-		
+
 	}
 
 	@Override
 	public void deleteShain() {
-		
+
 	}
 
 }

e41267c8f09cc7b13034471b7930e519cd10e1b5
uetak330 <uetak330@gmail.com>
2024-09-30 16:58:27 +0900

    インターフェースに抽象メソッドを追加

--- Spring05JDBC/src/main/java/uetak/Repository/ShainRepository.java
+++ Spring05JDBC/src/main/java/uetak/Repository/ShainRepository.java
@@ -7,5 +7,18 @@ import uetak.entity.Shain;
 public interface ShainRepository {
 	//社員リストを取得
 	ArrayList<Shain> findAll();
+	
+	//社員を登録
+	void insertShain();
+	
+	//社員を取得
+	void findByShain();
+	
+	//社員データの更新
+	void updateShain();
+	
+	//社員のデータを削除
+	void deleteShain();
+	
 
 }

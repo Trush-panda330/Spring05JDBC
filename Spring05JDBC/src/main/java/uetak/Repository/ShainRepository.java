f284b1b8d838306a2f23221c5b2292cb0636676b
uetak330 <uetak330@gmail.com>
2024-09-30 17:00:08 +0900

    findAll()の戻り値をvoidに

--- Spring05JDBC/src/main/java/uetak/Repository/ShainRepository.java
+++ Spring05JDBC/src/main/java/uetak/Repository/ShainRepository.java
@@ -1,12 +1,8 @@
 package uetak.Repository;
 
-import java.util.ArrayList;
-
-import uetak.entity.Shain;
-
 public interface ShainRepository {
 	//社員リストを取得
-	ArrayList<Shain> findAll();
+	void findAll();
 	
 	//社員を登録
 	void insertShain();

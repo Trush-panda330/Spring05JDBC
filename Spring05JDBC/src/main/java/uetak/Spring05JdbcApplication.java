321249e37fb92b6b0e433ccdcf0effbe5428304c
uetak330 <uetak330@gmail.com>
2024-09-30 16:40:58 +0900

    DIコンテナを作成　Repository をインスタンス化

--- Spring05JDBC/src/main/java/uetak/Spring05JdbcApplication.java
+++ Spring05JDBC/src/main/java/uetak/Spring05JdbcApplication.java
@@ -2,12 +2,24 @@ package uetak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import uetak.Repository.ShainRepository;
+import uetak.Repository.ShainRepositoryImpl;
 
 @SpringBootApplication
 public class Spring05JdbcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Spring05JdbcApplication.class, args);
+	public static void main(String[] args) {		
+		//DIコンテナを作成
+		ConfigurableApplicationContext context = 
+				SpringApplication.run(SpringApplication.class, args);
+		
+		//社員リポジトリのインスタンスを呼ぶ
+		ShainRepository shainRepository = context.getBean(ShainRepositoryImpl.class);
+		
+		//社員リストを取得
+		shainRepository.findAll();
 	}
 
 }
